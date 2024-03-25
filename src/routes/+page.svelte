944f994fc6fe01e2ccb35254e5cc7950f1ee266f
Jamie Chen <jamiechen2301@gmail.com>
2024-03-25 23:20:23 +1000

    Initial Commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<span></span>
+<div></div>
+
+<!-- Made with Svelte Center Footer-->
+<body>
+  <footer>
+    <p>Made with Svelte</p>
+  </footer>
+</body>
